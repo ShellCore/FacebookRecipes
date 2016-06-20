b1b4300820cf41d47b8ce230d1b6d12af2ea255b
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-20 16:14:46 -0500

    Inicialización de Base de datos.
    - Se creó la clase que controlará el nombre y versión de la base de datos.
    - Se creó la entidad "Recipe", con las anotaciones necesarias para su manejo en la base de datos.
    - Se modificó la clase que extiende de aplication para que se inicialice o se destruya la base de datos.
    - Se cambió la versión de la biblioteca DBFlow, para poder inicializar la base de datos de acuerdo al contexto.

--- app/src/main/java/com/edx/shell/android/facebookrecipes/FacebookRecipesApp.java
+++ app/src/main/java/com/edx/shell/android/facebookrecipes/FacebookRecipesApp.java
@@ -3,15 +3,32 @@ package com.edx.shell.android.facebookrecipes;
 import android.app.Application;
 
 import com.facebook.FacebookSdk;
+import com.raizlabs.android.dbflow.config.FlowManager;
 
 public class FacebookRecipesApp extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
         initFacebook();
+        initDb();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        dbTearDown();
     }
 
     private void initFacebook() {
         FacebookSdk.sdkInitialize(this);
+    }
+
+    private void initDb() {
+        FlowManager.init(this);
+    }
+
+    private void dbTearDown() {
+        FlowManager.destroy();
     }
 }
