8fd6d0bc0ba985906bdf3f3b037c5572de4f454c
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-20 16:25:58 -0500

    Inicialización de Cliente y servicios de Retrofit.
    - Se crearon los clientes y servicios necesarios para Retrofit.

---
+++ app/src/main/java/com/edx/shell/android/facebookrecipes/api/RecipeService.java
@@ -0,0 +1,14 @@
+package com.edx.shell.android.facebookrecipes.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RecipeService {
+    @GET("search")
+    Call<RecipeSearchResponse> search(@Query("key") String key,
+                                      @Query("sort") String sort,
+                                      @Query("count") int count,
+                                      @Query("page") int page);
+
+}
