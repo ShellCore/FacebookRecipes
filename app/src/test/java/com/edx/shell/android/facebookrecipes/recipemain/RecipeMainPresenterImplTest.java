4f6c4ed4de4d89a90da50140754d79fc8963510b
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-07-05 22:06:37 -0500

    Prueba del presentador de Main
    - Se agregaron pruebas para las siguientes funciones del presentador:
      - onEventMainThread() (cuando hay error, cuando se manda evento next, y evento save)
      - imageReady()
      - imageError()
      - getView()

--- app/src/test/java/com/edx/shell/android/facebookrecipes/recipemain/RecipeMainPresenterImplTest.java
+++ app/src/test/java/com/edx/shell/android/facebookrecipes/recipemain/RecipeMainPresenterImplTest.java
@@ -3,14 +3,17 @@ package com.edx.shell.android.facebookrecipes.recipeMain;
 import com.edx.shell.android.facebookrecipes.BaseTest;
 import com.edx.shell.android.facebookrecipes.entities.Recipe;
 import com.edx.shell.android.facebookrecipes.libs.base.EventBus;
+import com.edx.shell.android.facebookrecipes.recipeMain.events.RecipeMainEvent;
 import com.edx.shell.android.facebookrecipes.recipeMain.ui.RecipeMainView;
 
 import org.junit.Test;
 import org.mockito.Mock;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class RecipeMainPresenterImplTest extends BaseTest {
 
@@ -25,6 +28,8 @@ public class RecipeMainPresenterImplTest extends BaseTest {
 
     @Mock
     Recipe recipe;
+    @Mock
+    RecipeMainEvent event;
 
     private RecipeMainPresenterImpl presenter;
 
@@ -74,5 +79,63 @@ public class RecipeMainPresenterImplTest extends BaseTest {
         verify(view).hideElements();
         verify(view).showProgressBar();
         verify(getNextInteractor).execute();
+    }
+
+    @Test
+    public void testOnEvent_hasError() throws Exception {
+        String errorMessage = "error";
+        when(event.getError()).thenReturn(errorMessage);
+
+        presenter.onEventMainThread(event);
+
+        assertNotNull(presenter.getView());
+        verify(view).hideProgressBar();
+        verify(view).onGetRecipeError(event.getError());
+    }
+
+    @Test
+    public void testOnNextEvent_setRecipeOnView() throws Exception {
+        when(event.getType()).thenReturn(RecipeMainEvent.NEXT_EVENT);
+        when(event.getRecipe()).thenReturn(recipe);
+
+        presenter.onEventMainThread(event);
+
+        assertNotNull(presenter.getView());
+        verify(view).setRecipe(event.getRecipe());
+    }
+
+    @Test
+    public void testOnSaveEvent_notifyViewAndCallGetNextRecipe() throws Exception {
+        when(event.getType()).thenReturn(RecipeMainEvent.SAVE_EVENT);
+
+        presenter.onEventMainThread(event);
+
+        assertNotNull(presenter.getView());
+        verify(view).onRecipeSaved();
+        verify(getNextInteractor).execute();
+    }
+
+    @Test
+    public void testImageError_updateUI() throws Exception {
+        String imageError = "error";
+
+        presenter.imageError(imageError);
+
+        assertNotNull(presenter.getView());
+        verify(view).onGetRecipeError(imageError);
+    }
+
+    @Test
+    public void testImageReady_updateUI() throws Exception {
+        presenter.imageReady();
+
+        assertNotNull(presenter.getView());
+        verify(view).hideProgressBar();
+        verify(view).showElements();
+    }
+
+    @Test
+    public void testGetView_returnsView() throws Exception {
+        assertEquals(view, presenter.getView());
     }
 }
