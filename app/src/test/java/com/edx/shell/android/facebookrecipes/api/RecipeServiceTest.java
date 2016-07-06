97344d75f7de9b83a76c00e059388487c15763f7
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-07-05 21:18:06 -0500

    Pruebas de servicios
    - Se agregaron 2 pruebas mas para revisar el caso en el que el servicio truene y un random dentro del rango establecido en el apk.

--- app/src/test/java/com/edx/shell/android/facebookrecipes/api/RecipeServiceTest.java
+++ app/src/test/java/com/edx/shell/android/facebookrecipes/api/RecipeServiceTest.java
@@ -3,17 +3,21 @@ package com.edx.shell.android.facebookrecipes.api;
 import com.edx.shell.android.facebookrecipes.BaseTest;
 import com.edx.shell.android.facebookrecipes.BuildConfig;
 import com.edx.shell.android.facebookrecipes.entities.Recipe;
+import com.edx.shell.android.facebookrecipes.recipeMain.RecipeMainRepository;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.Random;
+
 import retrofit2.Call;
 import retrofit2.Response;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -32,8 +36,8 @@ public class RecipeServiceTest extends BaseTest {
 
     @Test
     public void doSearch_getRecipeFromBackend() throws Exception {
-        String sort = "r";
-        int count = 1;
+        String sort = RecipeMainRepository.RECENT_SORT;
+        int count = RecipeMainRepository.COUNT;
         int page = 1;
         Call<RecipeSearchResponse> call = service.search(BuildConfig.FOOD_API_KEY, sort, count, page);
 
@@ -45,5 +49,41 @@ public class RecipeServiceTest extends BaseTest {
 
         Recipe recipe = recipeSearchResponse.getFirstRecipe();
         assertNotNull(recipe);
+    }
+
+    @Test
+    public void doSearch_getNoRecipeFromBackend() throws Exception {
+        String sort = RecipeMainRepository.RECENT_SORT;
+        int count = RecipeMainRepository.COUNT;
+        int page = 1000000000;
+        Call<RecipeSearchResponse> call = service.search(BuildConfig.FOOD_API_KEY, sort, count, page);
+
+        Response<RecipeSearchResponse> response = call.execute();
+        assertTrue(response.isSuccess());
+
+        RecipeSearchResponse recipeSearchResponse = response.body();
+        assertEquals(0, recipeSearchResponse.getCount());
+
+        Recipe recipe = recipeSearchResponse.getFirstRecipe();
+        assertNull(recipe);
+    }
+
+    @Test
+    public void doSearch_getRandomRecipeFromBackend() throws Exception {
+        String sort = RecipeMainRepository.RECENT_SORT;
+        int count = RecipeMainRepository.COUNT;
+        int page = new Random().nextInt(RecipeMainRepository.RECIPE_RANGE);
+        Call<RecipeSearchResponse> call = service.search(BuildConfig.FOOD_API_KEY, sort, count, page);
+
+        Response<RecipeSearchResponse> response = call.execute();
+        assertTrue(response.isSuccess());
+
+        RecipeSearchResponse recipeSearchResponse = response.body();
+        if (recipeSearchResponse.getCount() == 1) {
+            Recipe recipe = recipeSearchResponse.getFirstRecipe();
+            assertNotNull(recipe);
+        } else {
+            System.out.println("Invalid recipe, try again");
+        }
     }
 }
