f238950c2201cec3e9f1afcbcd68c9834f0ac3ff
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-07-07 10:08:37 -0500

    Pruebas para los botones del adaptador de la lista.
    - Se probó el comportamiento de los botones de cada elemento de la lista.
      - Agregar a favorito.
      - Borrar elemento.
      - Compartir en Facebook.
      - Mandar como mensaje en Facebook.

--- app/src/test/java/com/edx/shell/android/facebookrecipes/recipeList/adapters/RecipesAdapterTest.java
+++ app/src/test/java/com/edx/shell/android/facebookrecipes/recipeList/adapters/RecipesAdapterTest.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.edx.shell.android.facebookrecipes.BaseTest;
@@ -12,6 +13,9 @@ import com.edx.shell.android.facebookrecipes.R;
 import com.edx.shell.android.facebookrecipes.entities.Recipe;
 import com.edx.shell.android.facebookrecipes.libs.base.ImageLoader;
 import com.edx.shell.android.facebookrecipes.support.ShadowRecyclerViewAdapter;
+import com.facebook.share.model.ShareContent;
+import com.facebook.share.widget.SendButton;
+import com.facebook.share.widget.ShareButton;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +28,7 @@ import org.robolectric.internal.ShadowExtractor;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -44,11 +49,13 @@ public class RecipesAdapterTest extends BaseTest {
 
     private RecipesAdapter adapter;
     private ShadowRecyclerViewAdapter shadowAdapter;
+    private String URL;
 
     @Override
     public void setup() throws Exception {
         super.setup();
-        when(recipe.getSourceUrl()).thenReturn("http://www.google.com");
+        URL = "http://www.google.com";
+        when(recipe.getSourceUrl()).thenReturn(URL);
 
         adapter = new RecipesAdapter(imageLoader, recipes, onItemClickListener);
         shadowAdapter = (ShadowRecyclerViewAdapter) ShadowExtractor.extract(adapter);
@@ -99,5 +106,78 @@ public class RecipesAdapterTest extends BaseTest {
         TextView txtRecipeName = (TextView) view.findViewById(R.id.txt_recipe_name);
 
         assertEquals(recipeTitle, txtRecipeName.getText().toString());
+    }
+
+    @Test
+    public void testOnFavoriteClick_ShouldCallListener() throws Exception {
+        int positionToClick = 0;
+        boolean favorite = true;
+        when(recipe.getFavorite()).thenReturn(favorite);
+        when(recipes.get(positionToClick)).thenReturn(recipe);
+
+        shadowAdapter.itemVisible(positionToClick);
+        shadowAdapter.performItemClickOverViewInHolder(positionToClick, R.id.btn_favorite);
+
+        View view = shadowAdapter.getViewForHolderPosition(positionToClick);
+        ImageButton btnFav = (ImageButton) view.findViewById(R.id.btn_favorite);
+
+        assertEquals(favorite, btnFav.getTag());
+        verify(onItemClickListener).onFavorite(recipe);
+    }
+
+    @Test
+    public void testOnNonFavoriteClick_ShouldCallListener() throws Exception {
+        int positionToClick = 0;
+        boolean favorite = false;
+        when(recipe.getFavorite()).thenReturn(favorite);
+        when(recipes.get(positionToClick)).thenReturn(recipe);
+
+        shadowAdapter.itemVisible(positionToClick);
+        shadowAdapter.performItemClickOverViewInHolder(positionToClick, R.id.btn_favorite);
+
+        View view = shadowAdapter.getViewForHolderPosition(positionToClick);
+        ImageButton btnFav = (ImageButton) view.findViewById(R.id.btn_favorite);
+
+        assertEquals(favorite, btnFav.getTag());
+        verify(onItemClickListener).onFavorite(recipe);
+    }
+
+    @Test
+    public void testOnDeleteClick_ShouldCallListener() throws Exception {
+        int positionToClick = 0;
+        when(recipes.get(positionToClick)).thenReturn(recipe);
+
+        shadowAdapter.itemVisible(positionToClick);
+        shadowAdapter.performItemClickOverViewInHolder(positionToClick, R.id.btn_delete);
+
+        verify(onItemClickListener).onDelete(recipe);
+    }
+
+    @Test
+    public void testFBShareBind_shareContentSet() throws Exception {
+        int positionToShow = 0;
+        when(recipes.get(positionToShow)).thenReturn(recipe);
+
+        shadowAdapter.itemVisible(positionToShow);
+        View v = shadowAdapter.getViewForHolderPosition(positionToShow);
+        ShareButton btnShare = (ShareButton) v.findViewById(R.id.btn_share);
+
+        ShareContent shareContent = btnShare.getShareContent();
+        assertNotNull(shareContent);
+        assertEquals(URL, shareContent.getContentUrl().toString());
+    }
+
+    @Test
+    public void testFBSendBind_sendContentSet() throws Exception {
+        int positionToShow = 0;
+        when(recipes.get(positionToShow)).thenReturn(recipe);
+
+        shadowAdapter.itemVisible(positionToShow);
+        View v = shadowAdapter.getViewForHolderPosition(positionToShow);
+        SendButton btnSend = (SendButton) v.findViewById(R.id.btn_send);
+
+        ShareContent shareContent = btnSend.getShareContent();
+        assertNotNull(shareContent);
+        assertEquals(URL, shareContent.getContentUrl().toString());
     }
 }
